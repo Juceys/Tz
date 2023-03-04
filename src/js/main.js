647ec44f8a6fae1caae46575d314567d6a30a8b8
Juceys <oleg.panikov97@gmail.com>
2023-03-04 23:34:37 +0400

    touch

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 // Исходные данные по слайдеру (const)
-
 const
   sliderCard = document.querySelectorAll('.slider__card'),
   sliderLine = document.querySelector('.slider__line'),
@@ -8,8 +7,14 @@ const
   sliderBtnPrev = document.querySelector('.slider__btn-prev');
 
 // Переменные
-let sliderCount = 0,
-  sliderWidth;
+let
+  sliderCount = 0,
+  sliderWidth,
+  isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID;
 
 // Адаптивность слайдера
 window.addEventListener('resize', showSlide);
@@ -18,17 +23,15 @@ window.addEventListener('resize', showSlide);
 sliderBtnNext.addEventListener('click', nextSlide);
 sliderBtnPrev.addEventListener('click', prevSlide);
 
-
 // Функции ==================
 
 // Начать демонстрацию
-function demonstration() {
-  document.querySelector('.demonstration').style.display = "none";
+function demo() {
+  document.querySelector('.demo').style.display = "none";
   showSlide();
 };
-
 document.getElementById('demo').addEventListener('click', () => {
-  setTimeout(demonstration, 5000);
+  setTimeout(demo, 5000);
 })
 
 // Задает нужную ширину картинки и sliderLine
@@ -72,7 +75,78 @@ function thisSlide(index) {
 sliderDots.forEach((dot, index) => {
   dot.addEventListener('click', () => {
     sliderCount = index;
-    rollSlider();
     thisSlide(sliderCount);
+    setPositionByIndex();
   })
 })
+
+// touch
+// вешаем слушатель-события на каждый слайд
+
+sliderCard.forEach((slide, index) => {
+  // touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchend', touchEnd)
+  slide.addEventListener('touchmove', touchMove)
+
+  // откл меню при удержании
+  window.oncontextmenu = function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    return false
+  }
+})
+
+function getPositionX(event) {
+  return event.touches[0].clientX
+}
+
+// получаем индекс слайда, позицию клика
+function touchStart(index) {
+  return function (event) {
+    sliderCount = index
+    startPos = getPositionX(event)
+    isDragging = true
+    animationID = requestAnimationFrame(animation)
+  }
+}
+
+// текущее перемещенее
+function touchMove(event) {
+  if (isDragging) {
+    const currentPos = getPositionX(event)
+    currentTranslate = prevTranslate + currentPos - startPos
+  }
+}
+
+// получаем дельту, определеяем направление смещения
+function touchEnd() {
+  cancelAnimationFrame(animationID)
+  isDragging = false
+  const movedBy = currentTranslate - prevTranslate
+
+  // если перемещено достаточно негатива, то перейдите к следующему слайду, если он есть
+  if (movedBy < -100 && sliderCount < sliderCard.length - 1) sliderCount += 1
+
+  // если перемещено достаточно позитивное, то перейдите к следующему слайду, если он есть
+  if (movedBy > 100 && sliderCount > 0) sliderCount -= 1
+
+  setPositionByIndex();
+  thisSlide(sliderCount);
+}
+
+function animation() {
+  setSliderPosition()
+  if (isDragging) requestAnimationFrame(animation)
+}
+
+//  задаёт текущее перермещение
+function setPositionByIndex() {
+  currentTranslate = sliderCount * -window.innerWidth
+  prevTranslate = currentTranslate
+  setSliderPosition()
+}
+
+function setSliderPosition() {
+  sliderLine.style.transform = `translateX(${currentTranslate}px)`
+}
